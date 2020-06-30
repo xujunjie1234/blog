4839dde764312719379d2698c6e85e910d656e42
junjie <1936725526@qq.com>
2020-06-30 13:29:37 +0800

    wsgi

---
+++ wsgi.py
@@ -0,0 +1,11 @@
+from blog import create_app
+
+from dotenv import load_dotenv
+import os
+
+dotenv_path = os.path.join(os.path.dirname(__file__),".env")
+
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
+
+app = create_app('production')
